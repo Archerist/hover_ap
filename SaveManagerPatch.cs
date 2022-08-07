ab108d6ab8bc78bb2d0c4bdf378056fd94b75503
Archerist <yunuscemberci@gmail.com>
2022-08-07 12:47:39 +0300

    Patch SaveManager.AddItem

---
+++ SaveManagerPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HarmonyLib;
+using MelonLoader;
+namespace TestMod
+{
+
+    [HarmonyPatch(typeof(SaveManager), nameof(SaveManager.AddItem), new Type[] { typeof(Item), typeof(byte) })]
+    class SaveManagerPatch
+    {
+
+        static void Prefix(Item item, byte count)
+        {
+            MelonLogger.Msg("Prefix works");
+            MelonLogger.Msg("{0}, {1}", item.itemType, count);
+
+        }
+    }
+}
