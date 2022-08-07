ef6774792c4a0f82793741cc71b91d58ed62fb9c
Archerist <yunuscemberci@gmail.com>
2022-08-07 13:50:54 +0300

    patching AddItem (Item, bytes) does not work

--- SaveManagerPatch.cs
+++ SaveManagerPatch.cs
@@ -4,14 +4,14 @@ using MelonLoader;
 namespace TestMod
 {
 
-    [HarmonyPatch(typeof(SaveManager), nameof(SaveManager.AddItem), new Type[] { typeof(Item), typeof(byte) })]
+    [HarmonyPatch(typeof(SaveManager), nameof(SaveManager.AddItem), new Type[] { typeof(string), typeof(byte) })]
     class SaveManagerPatch
     {
 
-        static void Prefix(Item item, byte count)
+        static void Prefix(string itemName, byte count)
         {
             MelonLogger.Msg("Prefix works");
-            MelonLogger.Msg("{0}, {1}", item.itemType, count);
+            MelonLogger.Msg("{0}, {1}", itemName, count);
 
         }
     }
